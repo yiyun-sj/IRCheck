9c8de90bb20de78867edb0c43103997dbd461dcd
s32jia <s32jia@uwaterloo.ca>
2023-10-27 12:41:03 -0400

    Added some notes

--- dedup.h
+++ dedup.h
@@ -15,3 +15,23 @@ void dedup_query_to_concept();
 void dedup_eval(std::string query, Interpretation interp);
 
 #endif
+
+/*
+NOTES (slide deck):
+
+TOPIC: eliminate operations that remove duplicates
+
+STRATEGY: query rewriting (?)
+* Capture schema in CFD
+  ? convert create table w/ constraints to CFD?
+* rewrite to remove distinct in a (range restricted) query (replace w/ elim)
+  * then rewrite again to remove elim as well (?)
+* Query evaluation with EVAL(Q, I) ?
+
+INDEX:
+* CFD & DLF: feature based description logic families
+  * has a polynomial-time decidable algorithm
+  * Pf: feature paths; either Id or F_1, \cdot, F_n
+
+
+*/
