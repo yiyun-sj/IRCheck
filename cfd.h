fba750f1975ffd0e3808a514802f5f53421f2fc7
s32jia <s32jia@uwaterloo.ca>
2023-11-22 22:32:36 -0500

    update structs

--- cfd.h
+++ cfd.h
@@ -28,41 +28,41 @@ typedef struct cfd_node {
 } cfd_node;
 
 typedef struct cfd_concept {
-  enum cfd_grammar_type type;
+  enum cfd_grammar_type type = CONCEPT;
 
   unsigned size;
   char *id;
 } cfd_concept;
 
 typedef struct cfd_restriction {
-  enum cfd_grammar_type type;
+  enum cfd_grammar_type type = RESTRICTION;
 
-  cfd_concept *c;
   cfd_pf *path;
+  cfd_concept *c;
 } cfd_restriction;
 
 typedef struct cfd_negation {
-  enum cfd_grammar_type type;
+  enum cfd_grammar_type type = NEGATION;
 
   cfd_concept *c;
 } cfd_negation;
 
 typedef struct cfd_conjunction {
-  enum cfd_grammar_type type;
+  enum cfd_grammar_type type = CONJUNCTION;
 
   cfd_node *lhs;
   cfd_node *rhs;
 } cfd_conjunction;
 
 typedef struct cfd_equality {
-  enum cfd_grammar_type type;
+  enum cfd_grammar_type type = EQUALITY;
 
   cfd_pf *lhs;
   cfd_pf *rhs;
 } cfd_equality;
 
 typedef struct cfd_pfd {
-  enum cfd_grammar_type type;
+  enum cfd_grammar_type type = PFD;
 
   cfd_node *concept;
   cfd_pf *rhs;
@@ -71,7 +71,7 @@ typedef struct cfd_pfd {
 } cfd_pfd;
 
 typedef struct cfd_pfd_key {
-  enum cfd_grammar_type type;
+  enum cfd_grammar_type type = PFD_KEY;
 
   cfd_node *concept;
   cfd_node *rhs;
